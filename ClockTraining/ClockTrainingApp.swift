aab6811d21c5ec3cc87488d03335ac65584e9586
ksurikova <ksantisurikova@gmail.com>
2024-11-19 12:41:00 +0300

    Initial Commit

---
+++ ClockTraining/ClockTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockTrainingApp.swift
+//  ClockTraining
+//
+//  Created by Kseniia Surikova on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClockTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
